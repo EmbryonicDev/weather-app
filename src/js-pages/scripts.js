6f5fca7e90c25f68a82a613de85425dd3a249363
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-10 14:17:41 +0200

    Wind & humidity to page via loadPage();

--- src/js-pages/scripts.js
+++ src/js-pages/scripts.js
@@ -11,11 +11,15 @@ export const loadPage = {
     loadPage.getCity();
     loadPage.getCountry();
     loadPage.getDate();
+    loadPage.getWind();
+    loadPage.getHumidity();
   },
   cacheDom() {
     loadPage.input = document.querySelector('input');
     loadPage.cityName = document.querySelector('#cityDate h2');
     loadPage.date = document.querySelector('#cityDate h4');
+    loadPage.wind = document.querySelector('#wind .dayData');
+    loadPage.humidity = document.querySelector('#humidity .dayData');
   },
   getCity() {
     loadPage.cityName.innerText = loadPage.data.city;
@@ -27,6 +31,12 @@ export const loadPage = {
     const date = new Date();
     const formattedDate = format(date, 'eeee, d MMMM yyyy | H:mm');
     loadPage.date.innerText = formattedDate;
+  },
+  getWind() {
+    loadPage.wind.innerText = loadPage.data.forecast.current.wind_speed;
+  },
+  getHumidity() {
+    loadPage.humidity.innerText = `${loadPage.data.forecast.current.humidity}%`;
   },
 };
 

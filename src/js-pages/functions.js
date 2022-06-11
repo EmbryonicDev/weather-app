fd79c0909c68626e165482c5ff52158abdebf4b0
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-11 16:10:43 +0200

    New: celOrFah() to add temp symbol

--- src/js-pages/functions.js
+++ src/js-pages/functions.js
@@ -16,10 +16,18 @@ export function changeTimeZone(date, timeZone) {
 // Convert country code to full country name
 export const countryName = new Intl.DisplayNames(['en'], { type: 'region' });
 
+// Meters to km / mi
 export const getDistance = (distanceInMeters, unit) => {
   let returnValue;
   if (unit === 'metric') returnValue = `${Math.round(distanceInMeters / 1000)} km`;
   if (unit === 'imperial') returnValue = `${Math.round(distanceInMeters / 1609)} mi`;
+  return returnValue;
+};
+
+export const celOrFah = (temp, unit) => {
+  let returnValue;
+  if (unit === 'metric') returnValue = `${Math.round(temp)} C°`;
+  if (unit === 'imperial') returnValue = `${Math.round(temp)} F°`;
   return returnValue;
 };
 

9b49cc68dbfdd4332aa31916d796b8733b22caf0
EmbryonicDev <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-11 10:48:38 +0200

    Add getDistance() to functions.js
    Convert meters to miles / km

--- src/js-pages/functions.js
+++ src/js-pages/functions.js
@@ -16,4 +16,11 @@ export function changeTimeZone(date, timeZone) {
 // Convert country code to full country name
 export const countryName = new Intl.DisplayNames(['en'], { type: 'region' });
 
+export const getDistance = (distanceInMeters, unit) => {
+  let returnValue;
+  if (unit === 'metric') returnValue = `${Math.round(distanceInMeters / 1000)} km`;
+  if (unit === 'imperial') returnValue = `${Math.round(distanceInMeters / 1609)} mi`;
+  return returnValue;
+};
+
 export default { changeTimeZone };

4118b352451a783465b202f24827a32b0aa067fd
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-14 17:15:22 +0200

    Create IIFE for initial page load & activating temp toggle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // import { getForecast } from './js-pages/main-async';
+import { toggleCelFah } from './js-pages/dom';
 import { loadPage } from './js-pages/scripts';
 import './styles/modern-normalize.css';
 import './styles/style.css';
 
-loadPage.init('Moat', 'imperial');
-// loadPage.init();
+(async () => {
+  await loadPage.init();
+  toggleCelFah.init();
+})();

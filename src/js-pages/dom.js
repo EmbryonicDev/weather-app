d7febec5133d512d61232138f762f91e1e41865e
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-15 10:11:46 +0200

    Modify toggleCelFah{} to store unit
    When click->toggleBtn->store unit to local storage (const storedUnit)

--- src/js-pages/dom.js
+++ src/js-pages/dom.js
@@ -1,5 +1,7 @@
 import { loadPage } from './scripts';
 
+export const storedUnit = JSON.parse(localStorage.getItem('storedUnit')) || [];
+
 export const toggleCelFah = {
   init() {
     this.cacheDom();
@@ -13,8 +15,21 @@ export const toggleCelFah = {
     const celBtn = this.unitBtn[0];
     const fahBtn = this.unitBtn[1];
 
-    celBtn.addEventListener('click', loadPage.init.bind(loadPage.init.bind, this.city[0], 'metric'));
-    fahBtn.addEventListener('click', loadPage.init.bind(loadPage.init.bind, this.city[0], 'imperial'));
+    celBtn.addEventListener('click', () => {
+      this.storeUnit('metric');
+      this.cacheDom(); // get new city name after user input
+      loadPage.init(this.city[0], 'metric');
+    });
+    fahBtn.addEventListener('click', () => {
+      this.storeUnit('imperial');
+      this.cacheDom(); // get new city name after user input
+      loadPage.init(this.city[0], 'imperial');
+    });
+  },
+  storeUnit(selectedUnit) {
+    if (storedUnit[0]) storedUnit.pop();
+    storedUnit.push(selectedUnit);
+    localStorage.setItem('storedUnit', JSON.stringify(storedUnit));
   },
 };
 

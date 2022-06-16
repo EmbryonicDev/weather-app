288886826dec9e9ea74b69608ce29be983acdbda
djdevlam <95647817+EmbryonicDev@users.noreply.github.com>
2022-06-16 08:15:23 +0200

    Remove error styling from getData() & apply setErrorMsg()

--- src/js-pages/main-async.js
+++ src/js-pages/main-async.js
@@ -1,8 +1,8 @@
 import { storedCity } from './dom';
+import { setErrorMsg } from './functions';
 
 // eslint-disable-next-line consistent-return
 export const getData = async (location = 'Amsterdam', unit = 'metric') => {
-  const errorMsg = document.getElementById('error');
   const apiKey = '8d8571b1c93bd4e2325c718b62e874dc';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${unit}`;
 
@@ -19,12 +19,13 @@ export const getData = async (location = 'Amsterdam', unit = 'metric') => {
     const response2 = await fetch(url2, { mode: 'cors' });
     const forecast = await response2.json();
 
-    errorMsg.style.cssText = 'color: transparent; font-weight: bold; align-self: flex-start; background: transparent; padding: 3px; border-radius: 6px';
+    setErrorMsg('off');
     return {
       city, countryCode, unit, forecast,
     };
   } catch (err) {
-    errorMsg.style.cssText = 'color: yellow; font-weight: bold; align-self: flex-start; background: red; padding: 3px; border-radius: 6px';
+    setErrorMsg('on');
+    // Reset local storage after wrong city entered
     while (storedCity.length > 0) storedCity.pop();
     localStorage.setItem('storedCity', JSON.stringify(storedCity));
     console.error('City Not Found');
